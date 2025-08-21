88d5416d6c7437156c5bc3950ab3978435e15362
Panchita <franciscalandwehr@hotmail.com>
2025-08-21 09:28:27 -0500

    changed booking page

--- src/app/booking/page.jsx
+++ src/app/booking/page.jsx
@@ -1,7 +1,36 @@
+'use client'
+import { useState } from "react";
 import ChooseDate from '../ui/components/calendar'
+import BookingForm from '../ui/components/form'
 
 export default function Location(){
+
+    const[selectedDay,setSelectedDay] = useState(null)
+    const[selectedTime, setSelectedTime] =useState(null)
+
+    const handleDateSelected = (date) =>{
+        setSelectedDay(date)
+    }
+
+    const handleTimeSelected = (time) =>{
+        setSelectedTime(time)        
+    }
+
+
+    if(selectedDay && selectedTime){
+        return(
+            <BookingForm 
+                date={selectedDay}
+                time={selectedTime}/>
+        )
+    }
     return(
-        <ChooseDate />
+        <>
+        <ChooseDate 
+            onDateSelected={handleDateSelected}
+            onTimeSelected={handleTimeSelected}
+            selectedDay={selectedDay}/>
+        <p className='mt-5 text-center'>Por Favor elige una fecha hora</p>
+        </>
     )
 }
--- src/app/ui/components/calendar.jsx
+++ src/app/ui/components/calendar.jsx
@@ -2,27 +2,22 @@
 
 import {Calendar} from "react-calendar";
 import { useState } from "react";
-import Link from 'next/link'
-
 import 'react-calendar/dist/Calendar.css'
 import TimePicker from './timePicker'
 
-export default function ChooseDate(){
-    const[value, setValue] = useState(new Date())
-    const[selectedDay,setSelectedDay] = useState(null)
-    const[selectedTime, setSelectedTime] =useState(null)
+export default function ChooseDate({onDateSelected, onTimeSelected, selectedDay, selectedTime}){
+    const[value, setValue] = useState(new Date())    
 
     let minDate = new Date()
     let maxDate = new Date(minDate)
     maxDate.setMonth(maxDate.getMonth()+1)
 
     function handleClickDay(date){    
-        setSelectedDay(date);
-        setSelectedTime(null)
-        
+        onDateSelected(date);
+        onTimeSelected(null)
     }
     
-    const disableMondays = ({date, view}) => date.getDay() === 1
+    const disableMondays = ({date}) => date.getDay() === 1
     
 
     return(
@@ -41,21 +36,10 @@ export default function ChooseDate(){
         {selectedDay &&
         <div className='text-center'>
             <TimePicker 
-                onSelectTime={setSelectedTime}
+                onSelectTime={onTimeSelected}
                 selectedTime={selectedTime} />
         </div>
         }
-        {selectedDay && selectedTime ?(
-            <div className='text-center mt-8'>
-            <p className=''>Tu Cita es el {selectedDay.toLocaleDateString()} a las {selectedTime}</p>
-            <Link href='/bookingForm'>
-            <button className='mt-8 lg:hover:cursor-pointer p-2 rounded transform transition-transform duration-300 lg:hover:scale-105 border-1 rounded active:bg-gray-800'>confirmar</button> 
-            </Link>
-            </div>
-            ):(
-             <p className='text-center mt-8'>por favor elige una fecha y hora</p>
-             )}
-        
         </>
     )
 }
