bddc9b8f1e2897ca5a8aba85de22c4b38adb8743
Panchita <franciscalandwehr@hotmail.com>
2025-08-23 20:52:04 -0500

    added fullyBookedDays api to disable fully booked days in the calendar

--- src/app/ui/components/calendar.jsx
+++ src/app/ui/components/calendar.jsx
@@ -1,12 +1,13 @@
 'use client'
 
 import {Calendar} from "react-calendar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import 'react-calendar/dist/Calendar.css'
 import TimePicker from './timePicker'
 
 export default function ChooseDate({onDateSelected, onTimeSelected, selectedDay, selectedTime}){
-    const[value, setValue] = useState(new Date())    
+    const[value, setValue] = useState(new Date())
+    const[disableDays, setDisableDays] = useState([])    
 
     let minDate = new Date()
     let maxDate = new Date(minDate)
@@ -17,7 +18,34 @@ export default function ChooseDate({onDateSelected, onTimeSelected, selectedDay,
         onTimeSelected(null)
     }
     
-    const disableMondays = ({date}) => date.getDay() === 1
+    //const disableMondays = ({date}) => date.getDay() === 1
+
+    useEffect(()=>{
+ 
+    const fetchDisabled = async() => {
+        try{
+            const res = await fetch(`/api/fullyBookedDays`)
+            const data = await res.json()
+            setDisableDays(data.fullyBookedDays)
+        }catch(error){
+            console.log(error)
+        }
+    }
+    fetchDisabled()
+    console.log('Disabled Days are : ', disableDays)
+    },[])
+
+    const disableDay = ({date, view}) =>{
+        if(view === 'month'){
+            if(date.getDay()===1){
+            return true
+            }
+            if(disableDays.includes(date.toISOString().split('T')[0])){
+                return true
+            }
+            return false
+        }
+    }
     
 
     return(
@@ -30,7 +58,8 @@ export default function ChooseDate({onDateSelected, onTimeSelected, selectedDay,
                 minDate ={minDate}
                 maxDate={maxDate} 
                 onClickDay={handleClickDay}
-                tileDisabled={disableMondays}/>
+                tileDisabled={disableDay}
+                />
         </div>
 
         {selectedDay &&
