85f14f58d38323437bacd6d0ad9712cbf17d91bc
Panchita <franciscalandwehr@hotmail.com>
2025-09-01 08:47:55 -0500

    added trash icon to clients card

--- src/app/ui/components/molecules/cards.jsx
+++ src/app/ui/components/molecules/cards.jsx
@@ -1,6 +1,9 @@
 'use client'
 
 import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function Card({title, children, className}){
 
@@ -16,7 +19,7 @@ export default function Card({title, children, className}){
             <div className='flex flex-col items-center mb-4 font-mono min-w-100'>
                 <div className="border rounded p-4 max-w-md w-full shadow-md">
                     <div className="flex justify-between items-center mb-2">
-                        <h2 className="text-lg">{title}</h2>
+                        <h2 className="text-lg">{title} <FontAwesomeIcon icon={faTrash} className='text-sm'/> </h2>
                         <button 
                             onClick={handleClick} 
                             className="border rounded text-sm w-6 h-6 flex items-center justify-center md:cursor-pointer" 
